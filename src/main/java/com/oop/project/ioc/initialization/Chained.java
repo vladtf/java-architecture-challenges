ded3c0a9cff49631f5c71c5930b13317f946482e
vladtf <vlad.tiftilov@yahoo.com>
2022-09-26 01:07:02 +0300

    add todo for Chained

--- src/main/java/com/oop/project/ioc/initialization/Chained.java
+++ src/main/java/com/oop/project/ioc/initialization/Chained.java
@@ -1,5 +1,8 @@
 package com.oop.project.ioc.initialization;
 
+
+// todo this may become an alternative for builder pattern by chaining decorators instead of just passing previous implementation
+//  to decorator class as an constructor argument
 public interface Chained {
     <T> T nextElement();
 }

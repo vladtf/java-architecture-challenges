be67bbba2c2e71977947a0a1eae0757426c820f1
vladtf <vlad.tiftilov@yahoo.com>
2022-09-26 00:57:48 +0300

    fixed class scanner

--- src/main/java/com/oop/project/ioc/initialization/DependentComponentScanner.java
+++ src/main/java/com/oop/project/ioc/initialization/DependentComponentScanner.java
@@ -10,6 +10,7 @@ import java.lang.reflect.Constructor;
 import java.util.*;
 import java.util.stream.Collectors;
 
+// TODO this class may have some issues with prototype beans
 @Log4j2
 public class DependentComponentScanner implements ComponentsScanner {
 
@@ -34,54 +35,48 @@ public class DependentComponentScanner implements ComponentsScanner {
 
     private Set<Class<?>> scanPrefix(String prefix) {
         Reflections reflections = new Reflections(prefix, new SubTypesScanner(false));
+        HashSet<Class<?>> classesInPrefix = new HashSet<>(reflections.getSubTypesOf(Object.class));
 
-        return new HashSet<>(reflections.getSubTypesOf(Object.class)).stream().filter(aClass -> {
-            Bean annotation = aClass.getAnnotation(Bean.class);
-
-            if (annotation != null) { // filter not beans
-                return !annotation.lazy() && !annotation.prototype(); // filter lazy beans and prototype
-            }
-            return false;
-        }).collect(Collectors.toSet());
-
+        return classesInPrefix.stream()
+                .filter(this::isNotAnnotation)
+                .filter(this::isBeanAnnotated)
+                .map(this::scanClass)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 
-    public Optional<Class<?>> scanClass(Class<?> clazz) {
-        if (instantiationStack.contains(clazz)) {
-            throw new IllegalStateException("Bean is already instantiating. This means that there might be a circular dependency, instantiation stack: " + instantiationStack);
+    private boolean isBeanAnnotated(Class<?> aClass) {
+        return aClass.isAnnotationPresent(Bean.class);
+    }
+
+    private boolean isNotAnnotation(Class<?> aClass) {
+        return !aClass.isAnnotation();
+    }
+
+    public Set<Class<?>> scanClass(Class<?> clazz) {
+        if (instantiationStack.contains(clazz) || !clazz.isAnnotationPresent(Bean.class)) {
+            return Collections.emptySet();
         }
 
-        if (clazz.isAnnotation()) {
-            return Optional.empty();
-        }
+        HashSet<Class<?>> result = new HashSet<>();
 
+        result.add(clazz);
         instantiationStack.add(clazz);
 
-        if (!clazz.isAnnotationPresent(Bean.class)) {
-            throw new IllegalArgumentException(clazz.getName() + " is not a Bean annotated class and cannot be instantiated.");
+        Constructor<?> constructor = getConstructor(clazz);
+
+        Class<?>[] constructorParameterTypes = constructor.getParameterTypes();
+
+        for (Class<?> constructorParameterType : constructorParameterTypes) {
+            result.addAll(scanClass(constructorParameterType));
         }
 
-        try {
+        log.debug("Finished scanning class: {}", clazz.getName());
 
-            Constructor<?> instance = getConstructor(clazz, args);
-
-            Bean annotation = clazz.getAnnotation(Bean.class);
-            if (!annotation.prototype()) { // don't store prototype beans
-                instance = putDependency(clazz, instance).orElse(null);
-            }
-
-            log.info("Registered new bean: {}", clazz.getName());
-
-            instantiationStack.remove(clazz);
-            return Optional.ofNullable(instance);
-        } catch (Exception e) {
-            log.info("Failed to instantiate bean: {}", clazz.getName(), e);
-        }
-
-        return Optional.empty();
+        return result;
     }
 
-    private Constructor<?> getConstructor(Class<?> clazz) throws NoSuchMethodException {
+    private Constructor<?> getConstructor(Class<?> clazz) throws RuntimeException {
 
         Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
 
@@ -94,25 +89,13 @@ public class DependentComponentScanner implements ComponentsScanner {
                 .toList();
 
         if (autowiredConstructors.size() == 0) {
-            throw new NoSuchMethodException("Multiple constructors, but no @Autowired constructor for class [" + clazz + "]");
-
+            throw new RuntimeException("Multiple constructors, but no @Autowired constructor for class [" + clazz + "]");
         }
 
         if (autowiredConstructors.size() > 1) {
-            throw new NoSuchMethodException("There are multiple constructors annotated with @Autowired for class [" + clazz + "]");
+            throw new RuntimeException("There are multiple constructors annotated with @Autowired for class [" + clazz + "]");
         }
-
 
         return autowiredConstructors.get(0);
     }
-
-    private <T> Optional<T> putDependency(Class<T> clazz, Object obj) {
-        // TODO what is this for
-        if (obj == null) {
-            return Optional.empty();
-        }
-
-        return Optional.of((T) obj);
-    }
-
 }
